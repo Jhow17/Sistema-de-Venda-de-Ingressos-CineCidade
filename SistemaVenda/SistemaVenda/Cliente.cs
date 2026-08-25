8ab38401a0e9a189719cadb729eb41926411c888
Jhow17 <silvajonathan234@gmail.com>
2026-08-25 08:20:55 -0300

    Adicionando projeto inicial

---
+++ SistemaVenda/SistemaVenda/Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public int Ingressos { get; set; } 
+
+        public string CDesconto { get; set; }
+
+
+        public int Idade { get; set; }
+
+        public double TaxaConveniencia = 5.0;
+
+
+        public double CalcularBruto()
+        {
+
+            //O Custo Bruto: R$ 5,00 de taxa de conveniência/reserva + R$ 25,00 por ingresso comprado.
+
+            return TaxaConveniencia + 25 * Ingressos;
+
+        }
+
+
+        public double CalcularLiquido()
+        {
+
+            //O Desconto: Se o cupom digitado for igual a "CINEMA10" (independente de maiúsculas/minúsculas), conceder 10% de desconto sobre o custo bruto; caso contrário, o desconto é R$ 0,00.
+            if (CDesconto.ToUpper() == "CINEMA10")
+            {
+                return CalcularBruto() - CalcularBruto() * 0.1;
+            }
+            else { 
+                return CalcularBruto(); 
+            }
+
+            
+
+        }
+    }
+
+
+  
+    }
---
+++ SistemaVenda/SistemaVenda/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SistemaVenda;
+
+Cliente clienteJose = new Cliente();
+
+
+Console.WriteLine("Qual seu Nome: ");
+clienteJose.Nome = Console.ReadLine();
+
+
+Console.WriteLine("Quantos Ingressos quer comprar: ");
+clienteJose.Ingressos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Tem Cupom de Desconto (Digite o cupom ): ");
+clienteJose.CDesconto = (Console.ReadLine());
+
+Console.WriteLine("Idade: ");
+clienteJose.Idade = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(clienteJose.Nome.ToLower());
+
+Console.WriteLine($"O valor Bruto vai ficar: {clienteJose.CalcularBruto()}");
+
+
+Console.WriteLine($" O valor Liquido vai ficar: {clienteJose.CalcularLiquido()}");
+
+
+
+Console.WriteLine($"Elegível para Sessão Noturna : {clienteJose.Idade >= 18}");
